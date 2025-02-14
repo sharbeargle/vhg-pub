75b77b74ccd39bb8072bae8dd2541c6c4aea33c6
Vincent Gov <vincehgov@gmail.com>
2025-02-14 15:28:22 +0000

    Revised API

--- src/rust/simpleargs/src/lib.rs
+++ src/rust/simpleargs/src/lib.rs
@@ -1,12 +1,28 @@
-
-/// Specify what type the flag value should be
+/// Specify what type the argument value should be
 #[derive(PartialEq, Eq, Debug)]
-pub enum FlagValueType {
+pub enum ArgType {
     Boolean,
     Character,
     Float,
     Integer,
     String,
+}
+
+pub struct FlagConfig {
+    name: String,
+    shortFlag: char,
+    longFlag: String,
+    required: bool,
+    hasArg: bool,
+    argType: Option<ArgType>,
+    description: String,
+}
+
+pub struct ArgConfig {
+    name: String,
+    argType: Option<ArgType>,
+    required: bool,
+    description: String,
 }
 
 // TODO: Define and implement how configuration will be stored
@@ -15,59 +31,60 @@ pub struct Parser {
 }
 
 pub fn new(description: String) -> Parser {
-
-    Parser{description}
+    Parser { description }
 }
 
+// TODO: decide how flags syntax
+// Should it be -f=<arg>, or -f <arg>, or -f<arg>
+// Maybe support all of those cases
 impl Parser {
     /// Add a flag to the configuration
     /// A flag with no value is a boolean.
-    pub fn add_flag(mut self, name: String, short_flag: Option<char>, long_flag: Option<String>, has_value: bool, value_type: Option<FlagValueType>, description: String) -> Self {
-
+    pub fn add_flag(mut self, flag_config: FlagConfig) -> Self {
         self
     }
 
     /// Add a positional argument to the configuration
-    /// All positional arguments will be parsed as strings.
-    pub fn add_positional_args(mut self, name: String, minimum_required: u32, description: String) -> Self {
-
+    /// Parsed in order added. Adding required args after unrequired args will have undefined behavior.
+    /// arg_type: Not used right now. Just put None.
+    pub fn add_arg(mut self, arg_config: ArgConfig) -> Self {
         self
     }
 
     /// Print the help screen
-    pub fn print_help(&self) {
-
-    }
+    pub fn print_help(&self) {}
 
     /// Parse the command line arguments
     /// Validate configuration.
     pub fn parse(mut self) -> Self {
+        // TODO: define how the flags will be Parsed
+        let mut args = std::env::args();
 
         self
     }
 
-    pub fn get_boolean_flag(&self, name: &str) -> Option<bool> {
+    pub fn get_flag_as_boolean(&self, name: &str) -> Option<bool> {
         None
     }
 
-    pub fn get_character_flag(&self, name: &str) -> Option<char> {
+    pub fn get_flag_as_character(&self, name: &str) -> Option<char> {
         None
     }
 
-    pub fn get_integer_flag(&self, name: &str) -> Option<i32> {
+    pub fn get_flag_as_integer(&self, name: &str) -> Option<i32> {
         None
     }
 
-    pub fn get_float_flag(&self, name: &str) -> Option<f32> {
+    pub fn get_flag_as_float(&self, name: &str) -> Option<f32> {
         None
     }
 
-    pub fn get_string_flag(&self, name: &str) -> Option<&str> {
+    pub fn get_flag_as_string(&self, name: &str) -> Option<&str> {
         None
     }
 
-    pub fn get_positional_args(&self) -> Option<&Vec<String>> {
-
+    /// Get all positional argument
+    pub fn get_arg_as_string(&self, name: &str) -> Option<&Vec<String>> {
         None
     }
 }
@@ -77,7 +94,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-
-    }
+    fn it_works() {}
 }
